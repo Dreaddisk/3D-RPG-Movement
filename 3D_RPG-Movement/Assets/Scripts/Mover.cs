c71cde0ff1527330a85f0258c48c3d0e8df796ee
Jose <joey_manuel@outlook.com>
2021-02-18 08:31:54 -0500

    6. Navmesh
    created and redefined the Nav Mesh AI path.

---
+++ 3D_RPG-Movement/Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private Transform target;
+
+
+    
+    #endregion
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+
+
+
+} // Mover class
