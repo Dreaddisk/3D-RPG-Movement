ecf653770d4c7a08ecd3e2f9cdf3c021b07ae9f4
Jose <joey_manuel@outlook.com>
2021-02-20 15:45:38 -0500

    8. Camera Follow
    Created a a CameraFollow script so the camera follows the player.

---
+++ 3D_RPG-Movement/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    #region Variables
+    [SerializeField]
+    private Transform target;
+
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
